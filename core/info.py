350596df0e687614cad304e4de65abd66a7d2fe2
KmolYuan <pyslvs@gmail.com>
2018-11-12 13:47:11 +0800

    Update version.

--- core/info.py
+++ core/info.py
@@ -6,7 +6,7 @@ __author__ = "Yuan Chang"
 __copyright__ = "Copyright (C) 2018"
 __license__ = "AGPL"
 __email__ = "pyslvs@gmail.com"
-__version__ = "18.11"
+__version__ = "18.11.1"
 
 from sys import version_info
 import platform
